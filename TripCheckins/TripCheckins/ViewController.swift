9144b41460f7dcb0e8046e759994ad2d72ba8a5a
Nataliya Patsovska <nataliya.patsovska@gmail.com>
2017-08-08 14:51:39 +0300

    Create TripCheckins project

---
+++ TripCheckins/TripCheckins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripCheckins
+//
+//  Created by Nataliya  on 8/8/17.
+//  Copyright © 2017 Nataliya Patsovska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
